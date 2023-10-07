244c1b8ebcb7afeb5a4482b38c48304129814ff7
javav12 <120237569+javav12@users.noreply.github.com>
2023-10-07 19:29:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,18 +31,23 @@ async def information(ctx):
     await ctx.send(bilgi_mesaji)
 
 
-# Geri dönüşüm komutunu oluşturun
+
 @bot.command()
 async def recycling(ctx):
     geri_donusum_mesaji = "Recycling is the process of converting waste materials into new materials and objects. This concept often includes the recovery of energy from waste materials. The recyclability of a material depends on its ability to reacquire the properties it had in its original state.[1] It is an alternative to 'conventional' waste disposal that can save material and help lower greenhouse gas emissions. It can also prevent the waste of potentially useful materials and reduce the consumption of fresh raw materials, reducing energy use, air pollution (from incineration) and water pollution (from landfilling).  source: wikipedia"
     await ctx.send(geri_donusum_mesaji)
 
 
-# Enerji tasarrufu komutunu oluşturun
+
 @bot.command()
 async def energy_saving(ctx):
     enerji_tasarrufu_mesaji = "You can make small changes at home and at work to save energy. For example, remember to turn off the lights and turn off unnecessary electronics💡."
     await ctx.send(enerji_tasarrufu_mesaji)
+
+@bot.command()
+async def Electric_cars_are_environmentally_friendly(ctx):
+    eliktirikliarac = "Battery production for environmentally friendly unmanned cars is not that environmentally friendly"
+    await ctx.send(eliktirikliarac)
 
 
 bot.run(dosya)
